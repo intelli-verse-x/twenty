d784f1ce0c10e904675ba578cce3964e164a38ad
Bhavesh <bhaveshanjana58@gmail.com>
2026-09-02 22:54:59 +0530

    ui(crm): replace Twenty product name with CRM in the app and emails

--- packages/twenty-front/src/pages/auth/SignInUp.tsx
+++ packages/twenty-front/src/pages/auth/SignInUp.tsx
@@ -136,7 +136,7 @@ export const SignInUp = () => {
     }
 
     if (isGlobalScope) {
-      return t`Welcome to Twenty`;
+      return t`Welcome to CRM`;
     }
 
     const workspaceName = workspacePublicData?.displayName;
